af6ae465681d78980dd994a530c07fcd18b89dfb
breadbutterjam <jaimeen@gmail.com>
2023-01-09 18:03:35 +0530

    Add files via upload
    added checking functionality. 
    [pending] allow retaking wrong ans
    [pending] show wrong states in different color (for a short time interval)
    [pending] check issue of alert appearing before color change of state

--- code.js
+++ code.js
@@ -1,10 +1,13 @@
-let dataState = {"mh":["Maharashtra","mumbai"],"gj":["gujarat","gandhinagar"],"jh":["Jharkhand","Ranchi","Jharkhand","Ranchi"],"up":["UP","Lucknow"],"hp":["Himachal","Shimla"],"kl":["Kerala","Trivandrum"],"ka":["Karnataka","Banglore"],"ga":["Goa","Panaji"],"tn":["Tamil Nadu","Chennai"],"ap":["Andhra Pradesh","Amravati"],"tg":["Telangana","Hyderabad"],"ct":["Chattisgarh","Raipur"],"or":["Oddisha","Bhubaneswar"],"br":["Bihar","Patna"],"mp":["Madhya Pradesh"],"rj":["Rajasthan","Jaipur"],"pb":["Punjab","Chandigarh"],"hr":["Haryana","Chandigarh"],"ut":["Uttarakhand","Dehradun"],"sk":["Sikkim","Gangtok"],"wb":["West Bengal","Kolkatta"],"ar":["Arunachal Pradesh","Itanagar"],"as":["Assam","Dispur"],"nl":["Nagaland","Kohima"],"mn":["Manipur","Imphal"],"mz":["Mizoram","Aizawl"],"tr":["Tripura","Agartala"],"ml":["Meghalaya","Shillong"]}
+let dataState2 = {"mh":["Maharashtra","mumbai"],"gj":["gujarat","gandhinagar"],"jh":["Jharkhand","Ranchi","Jharkhand","Ranchi"],"up":["UP","Lucknow"],"hp":["Himachal","Shimla"],"kl":["Kerala","Trivandrum"],"ka":["Karnataka","Banglore"],"ga":["Goa","Panaji"],"tn":["Tamil Nadu","Chennai"],"ap":["Andhra Pradesh","Amravati"],"tg":["Telangana","Hyderabad"],"ct":["Chattisgarh","Raipur"],"or":["Oddisha","Bhubaneswar"],"br":["Bihar","Patna"],"mp":["Madhya Pradesh"],"rj":["Rajasthan","Jaipur"],"pb":["Punjab","Chandigarh"],"hr":["Haryana","Chandigarh"],"ut":["Uttarakhand","Dehradun"],"sk":["Sikkim","Gangtok"],"wb":["West Bengal","Kolkatta"],"ar":["Arunachal Pradesh","Itanagar"],"as":["Assam","Dispur"],"nl":["Nagaland","Kohima"],"mn":["Manipur","Imphal"],"mz":["Mizoram","Aizawl"],"tr":["Tripura","Agartala"],"ml":["Meghalaya","Shillong"]}
+
+let dataState = {"mh":["Maharashtra","mumbai"],"gj":["gujarat","gandhinagar"],"kl":["Kerala","Trivandrum"],"ka":["Karnataka","Banglore"],"ga":["Goa","Panaji"],"tn":["Tamil Nadu","Chennai"]}
 
 let arrAllStates = Object.keys(dataState);
 let arrCorrect = [];
 let arrWrong = [];
 let arrDone = [];
 
+let NUM_STATES = arrAllStates.length - 1;
 
 // console.log("127");
 function init(){
@@ -18,6 +21,8 @@ function init(){
     //add event listenes
     addEventListeners();
 
+    getNextQuestion();
+
 }
 
 function addEventListeners(){
@@ -25,28 +30,80 @@ function addEventListeners(){
     // $('.cls-2').on("click", stateClicked2)
 }
 
+let currentStateIndex;
 
 function getNextQuestion(){
     if (arrDone.length === arrAllStates.length){
+        
+        /* if (arrWrong.length > 0){
+            let t = confirm("done. do you want to attempt the wrong ones again?")
+            if (t){
+                arrDone = JSON.parse(JSON.stringify(arrCorrect));
+                arrWrong = [];
+                getNextQuestion();
+            }
+        } else {
+            alert ("done. refresh page to start again.");
+        } */
+
         alert ("done. refresh page to start again.");
         return;
     } 
 
     let nCurrentIndex
     do {
-        t = getRandomNumber(27);
+        t = getRandomNumber(NUM_STATES);
     } while (arrDone.indexOf(t) > -1)
 
+    currentStateIndex = t; 
     arrDone.push(t);
     //$('.questionHolder').text(dataState["mh"][0])
     let strStateCode = arrAllStates[t];
     let strStateName = dataState[strStateCode][0]
-    $('.questionHolder').text(strStateName);
+    $('.questionHolder').text(strStateName);    
+
+
 }
 
 
 function stateClicked(event){
-    console.log(event.target.id);
+    //console.log(event.target.id);
+    let clickedStateId = event.target.id;
+    let correctAns = arrAllStates[currentStateIndex];
+
+    // console.log("clickedStateId", clickedStateId);
+    // console.log("correctAns", correctAns);
+
+
+    if (clickedStateId === correctAns){
+        // alert("correct")
+        console.log("correct");
+        markAns("correct");
+    } else {
+        console.log("wrong");
+        markAns("wrong");
+    }
+
+    getNextQuestion();
+}
+
+function markAns(param){
+    let t = arrDone.length - 1;
+    let elem = $('.bullet')[t];
+
+    let statePathID = arrAllStates[currentStateIndex];
+    let statePath = $("#" + statePathID)[0];
+
+    if (param === "correct"){
+        arrCorrect.push(currentStateIndex);
+        elem.classList.add("correctBullet");
+        statePath.style.fill = "lightgreen";
+
+    
+    } else if (param === "wrong"){
+        arrWrong.push(currentStateIndex);
+        elem.classList.add("wrongBullet");
+    }
 }
 
 function addBullets(parentElem, nCount){
